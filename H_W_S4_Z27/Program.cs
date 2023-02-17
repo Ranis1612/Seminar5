86c040e2039259c9f936cc67a5d986da901ab38b
Ranis1612 <Ranis1612@mail.ru>
2023-02-17 21:29:18 +0500

    Задания к семинару 5

---
+++ H_W_S4_Z27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {num} равна { GetSum(num)}");
+
+int GetSum(int num){
+  int sum = 0;
+  while(num > 0)
+  {
+    sum += num%10;
+    num /=10;
+  } 
+  return sum;
+}
---
+++ Z32/Program.cs
@@ -0,0 +1,28 @@
+﻿// **Задача 32:** Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = XZArray(5, -10,10);
+Console.WriteLine(String.Join(" ",array));
+
+int[] res =  InversArray(array);
+Console.WriteLine(String.Join(" ",res));
+
+
+int[] InversArray(int[] xyi)
+{
+for(int i =0; i < xyi.Length;i++){
+  xyi [i] *= -1; 
+}
+return xyi;
+}
+
+
+int[] XZArray(int size, int minValue, int maxValue)
+{
+  int[] res = new int[size];
+  for(int i = 0; i < size; i++){
+    res[i] = new Random().Next(minValue, maxValue+1);
+  }
+  return res;
+} 
+
